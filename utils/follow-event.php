5c0dfaab094b8d3e88b66a839ed16ace95c9cacc
Andrea Pulita <andrea.pulita@studio.unibo.it>
2020-01-07 19:38:45 +0100

    Fix follow

--- utils/follow-event.php
+++ utils/follow-event.php
@@ -1,20 +1,20 @@
 <?php
     require_once '../initializer.php';
     
-    if(($_POST['idUser'] == '') || $_POST['idEvent'] == '') {
+    if($_POST['idEvento'] == '') {
         echo json_encode(array('result' => 'warning', 'message' => 'Username o evento non corretto'));
-    } else {
-        $idUser = $_POST['idUser'];
-        $idEvent = $_POST['idEvent'];
+    } elseif(isSessionActive() == true){
+        $idUser = $_SESSION['idUtente'];
+        $idEvento = $_POST['idEvento'];
         if($_POST['follow'] == "true"){
-            $result = $dbh->follow($idUser, $idEvent);
+            $result = $dbh->follow($idUser, $idEvento);
             if ($result == true) {
                 echo json_encode(array('result' => 'ok', 'message' => 'Follow effettuato con successo!'));
             } else {
                 echo json_encode(array('result' => 'error', 'message' => 'Qualcosa non va!'));
             }
         } else {
-            $result = $dbh->unFollow($idUser, $idEvent);
+            $result = $dbh->unFollow($idUser, $idEvento);
             if ($result == true) {
                 echo json_encode(array('result' => 'ok', 'message' => 'Unfollow effettuato con successo!'));
             } else {
