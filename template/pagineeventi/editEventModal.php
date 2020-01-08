34d3ca9df9faa60358cfb439e57f28a30ad08557
Andrea Pulita <andrea.pulita@studio.unibo.it>
2020-01-08 19:49:26 +0100

    Accessibilita form fine

--- template/pagineeventi/editEventModal.php
+++ template/pagineeventi/editEventModal.php
@@ -9,33 +9,33 @@ $sessionData .= '
                     <legend class="uk-legend uk-text-center uk-margin-top">Modifica Evento</legend>
 
                     <div class="uk-margin">
-                        <label class="uk-form-label" for="form-stacked-text">Nome Evento
-                        <input required id="editNomeEvento" class="uk-input" type="text" placeholder="Inserisci nome Evento"></input></label>
+                        <label class="uk-form-label" for="editNomeEvento">Nome Evento</label>
+                        <input required id="editNomeEvento" class="uk-input" text="Nome Evento" type="text" placeholder="Inserisci nome Evento"></input>
                     </div>
 
                     <div class="uk-margin">
-                        <label class="uk-form-label" for="form-stacked-text">Artisti dell\' evento</br>
-                        <select style="width: 50%" id="editSelectIdArtista" class="selectArtista" multiple>
-                        </select></label>
+                        <label class="uk-form-label" for="editSelectIdArtista">Artisti dell\' evento</label></br>
+                        <select style="width: 50%" id="editSelectIdArtista" text="Artisti" class="selectArtista" multiple>
+                        </select>
                         <a href="#modal-addArtista" uk-toggle class="uk-toggle uk-float-right">
                             <button class="openModalAddArtista uk-icon-button uk-float-right uk-form-width-small" uk-icon="plus-circle"></button>
                         </a>
                     </div>
 
                     <div class="uk-margin">
-                        <label class="uk-form-label" for="form-stacked-text">Categorie dell\' evento</br>
-                        <select style="width: 60%" id="editSelectIdCategoria" class="selectCategoria" multiple>
-                        </select></label>
+                        <label class="uk-form-label" for="editSelectIdCategoria">Categorie dell\' evento</label></br>
+                        <select style="width: 60%" id="editSelectIdCategoria" text="Categorie Evento" class="selectCategoria" multiple>
+                        </select>
                     </div>
 
                     <div class="uk-margin">
-                        <label class="uk-form-label" for="form-stacked-text">Locandina evento</br>
-                        <input type="file" name="pathLocandina" id="editPathLocandina"></input></label>
+                        <label class="uk-form-label" for="editPathLocandina">Locandina evento</label></br>
+                        <input type="file" name="pathLocandina" id="editPathLocandina"></input>
                     </div>
 
                     <div class="uk-margin">
-                        <label class="uk-form-label" for="form-stacked-text">Luogo evento</br>
-                        <select style="width: 50%" id="editSelectIdLuogo" class="selectLuogo"></label>
+                        <label class="uk-form-label" for="editSelectIdLuogo">Luogo evento</label></br>
+                        <select style="width: 50%" id="editSelectIdLuogo" class="selectLuogo">
                         </select>
                         <a href="#modal-addLuogo" uk-toggle class="uk-toggle uk-float-right">
                             <button class="openModalAddLuogo uk-icon-button uk-float-right uk-form-width-small" uk-icon="plus-circle"></button>
@@ -43,20 +43,20 @@ $sessionData .= '
                     </div>
 
                     <div class="uk-margin">
-                        <label class="uk-form-label" for="form-stacked-text">Data inizio evento
+                        <label class="uk-form-label" for="editDataInizio">Data inizio evento</label>
                         <input required id="editDataInizio" class="dataInizio uk-input" type="text" 
-                            placeholder="Inserisci Data di inizio evento" data-input></input></label>
+                            placeholder="Inserisci Data di inizio evento" data-input></input>
                     </div>
 
                     <div class="uk-margin">
-                        <label class="uk-form-label" for="form-stacked-text">Data fine evento
+                        <label class="uk-form-label" for="editDataFine">Data fine evento</label>
                         <input required id="editDataFine" class="dataFine uk-input" type="text" 
-                            placeholder="Inserisci Data di fine evento" data-input></input></label>
+                            placeholder="Inserisci Data di fine evento" data-input></input>
                     </div>
 
                     <div class="uk-margin">
-                        <label class="uk-form-label" for="form-stacked-text">Descrizione
-                        <textarea required id ="editDescrizioneEvento" class="uk-textarea" rows="8" placeholder="Inserisci descrizione evento"></textarea></label>
+                        <label class="uk-form-label" for="editDescrizioneEvento">Descrizione</label>
+                        <textarea required id ="editDescrizioneEvento" class="uk-textarea" rows="8" placeholder="Inserisci descrizione evento"></textarea>
                     </div>
                     <div class="uk-width-1-1">
                         <button id="saveEventoBtn" type="submit" class="uk-button uk-button-primary uk-margin-left uk-width-1-3 uk-float-left">Salva</button>
